6f868be78712f251512724c8ba3ec9678f5f12e9
95kim1 <sunghee.k@kakao.com>
2022-04-10 19:07:33 +0900

    vector

---
+++ vector/shvector.c
@@ -0,0 +1,137 @@
+/************************************************
+* file name    : shvector.c
+* create date  : 2022.04.10
+* update date  : 2022.04.10
+* writer       : 95kim1 (sunghee.k)
+* last updater : 95kim1 (sunghee.k)
+* description  : Dynamic Array's source file
+*
+*   sh_vector's definitions of member functions
+*
+*************************************************/
+
+#include "shvector.h"
+
+
+  /////////////////
+ // Initiallize //
+/////////////////
+
+void init_sh_vector(sh_vector* vec) {
+    vec->_size = 0;
+    vec->_capaticty = 0;
+    vec->_data = NULL;
+
+    // Capacity
+    vec->size = _sh_size;
+    vec->capacity = _sh_capacity;
+    
+    // Access
+    vec->get = _sh_get;
+    vec->front = _sh_front;
+    vec->back = _sh_back;
+
+    // Modifier
+    vec->insert = _sh_insert;
+    vec->push_back = _sh_push_back;
+    
+    vec->erase = _sh_erase;
+    vec->erase_range = _sh_erase_range;
+    vec->pop_back = _sh_pop_back;
+
+    vec->clear = _sh_clear;
+    vec->free = _sh_free;
+
+    // Operation
+    vec->find = _sh_find;
+    vec->swap = _sh_swap;
+    vec->reverse = _sh_reverse;
+    vec->merge = _sh_merge;
+    vec->sort = _sh_sort;
+    vec->remove_if = _sh_remove_if;
+}
+void init_sh_vector_n(sh_vector* vec,  int capacity) {
+    init_sh_vector(vec);
+    vec->_capacity = capacity;
+}
+
+
+  //////////////
+ // Capacity //
+//////////////
+
+int _sh_size(struct sh_vector* self)  {
+    return self->_size;
+}
+int _sh_capacity(struct sh_vector* self)  {
+    return self->_capacity;
+}
+
+
+  ////////////
+ // Access //
+////////////
+
+void* _sh_get(struct sh_vector* self,  int index)  {
+    return (index > 0 && self->_size > index) ? self->_data[index] : NULL;
+}
+void* _sh_front(struct sh_vector* self)  {
+    return _sh_get(self, 0);
+}
+void* _sh_back(struct sh_vector* self)  {
+    return _sh_get(self, self->_size - 1);
+}
+
+
+  //////////////
+ // Modifier //
+//////////////
+
+bool _sh_insert(struct sh_vector* self, void* data,  int index)  {
+
+}
+bool _sh_push_back(struct sh_vector* self, void* data)  {
+
+}
+
+bool _sh_erase(struct sh_vector* self,  int index)  {
+
+}
+bool _sh_erase_range(struct sh_vector* self,  int start,  int end)  {
+
+}
+bool _sh_pop_back(struct sh_vector* self)  {
+
+}
+
+void _sh_clear(struct sh_vector* self)  {
+
+}
+void _sh_free(struct sh_vector* self)  {
+
+}
+
+
+  ///////////////
+ // Operation //
+///////////////
+
+int _sh_find(struct sh_vector* self, void* data, bool compare(void*, void*))  {
+
+}
+void _sh_swap(struct sh_vector* self, int a, int b)  {
+
+}
+void _sh_reverse(struct sh_vector* self)  {
+
+}
+void _sh_merge(struct sh_vector* self, struct sh_vector* other)  {
+
+}
+void _sh_sort(struct sh_vector* self)  {
+
+}
+void _sh_remove_if(struct sh_vector* self, void* data, bool compare(void*, void*))  {
+
+}
+
---
+++ vector/shvector.h
@@ -0,0 +1,87 @@
+/************************************************
+* file name    : shvector.h
+* create date  : 2022.04.10
+* update date  : 2022.04.10
+* writer       : 95kim1 (sunghee.k)
+* last updater : 95kim1 (sunghee.k)
+* description  : Dynamic Array's header file
+*
+*   sh_vector structure
+*   sh_vector's prototypes of member functions
+*
+*************************************************/
+
+#ifndef _SH_VECTOR_H_
+#define _SH_VECTOR_H_
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct sh_vector {
+     int _capacity;
+     int _size;
+    void** _data;
+///////
+    // Capacity
+    int size(struct sh_vector* self);
+    int capacity(struct sh_vector* self);
+    
+    // Access
+    void* get(struct sh_vector* self,  int index);
+    void* front(struct sh_vector* self);
+    void* back(struct sh_vector* self);
+
+    // Modifier
+    bool insert(struct sh_vector* self, void* data,  int index);
+    bool push_back(struct sh_vector* self, void* data);
+    
+    bool erase(struct sh_vector* self,  int index);
+    bool erase_range(struct sh_vector* self,  int start,  int end);
+    bool pop_back(struct sh_vector* self);
+
+    void clear(struct sh_vector* self);
+    void free(struct sh_vector* self);
+
+    // Operation
+    int find(struct sh_vector* self, void* data, bool compare(void*, void*));
+    void swap(struct sh_vector* self, int a, int b);
+    void reverse(struct sh_vector* self);
+    void merge(struct sh_vector* self, struct sh_vector* other);
+    void sort(struct sh_vector* self);
+    void remove_if(struct sh_vector* self, void* data, bool compare(void*, void*));
+} sh_vector;
+
+// Initiallize
+void init_sh_vector(sh_vector* vec);
+void init_sh_vector_n(sh_vector* vec,  int capacity);
+
+// Capacity
+int _sh_size(struct sh_vector* self);
+int _sh_capacity(struct sh_vector* self);
+
+// Access
+void* _sh_get(struct sh_vector* self,  int index);
+void* _sh_front(struct sh_vector* self);
+void* _sh_back(struct sh_vector* self);
+
+// Modifier
+bool _sh_insert(struct sh_vector* self, void* data,  int index);
+bool _sh_push_back(struct sh_vector* self, void* data);
+
+bool _sh_erase(struct sh_vector* self,  int index);
+bool _sh_erase_range(struct sh_vector* self,  int start,  int end);
+bool _sh_pop_back(struct sh_vector* self);
+
+void _sh_clear(struct sh_vector* self);
+void _sh_free(struct sh_vector* self);
+
+// Operation
+int _sh_find(struct sh_vector* self, void* data, bool compare(void*, void*));
+void _sh_swap(struct sh_vector* self, int a, int b);
+void _sh_reverse(struct sh_vector* self);
+void _sh_merge(struct sh_vector* self, struct sh_vector* other);
+void _sh_sort(struct sh_vector* self);
+void _sh_remove_if(struct sh_vector* self, void* data, bool compare(void*, void*));
+
+
+#endif
